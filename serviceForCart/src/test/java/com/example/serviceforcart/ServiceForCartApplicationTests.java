4cf9022aff93e36aaf8a1155071f3a48b082a0b9
OgyrechekSan <balybin.1988@mail.ru>
2026-03-22 23:03:35 +0300

    Fix builds

--- serviceForCart/src/test/java/com/example/serviceforcart/ServiceForCartApplicationTests.java
+++ serviceForCart/src/test/java/com/example/serviceforcart/ServiceForCartApplicationTests.java
@@ -2,8 +2,10 @@ package com.example.serviceforcart;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class ServiceForCartApplicationTests {
 
     @Test
